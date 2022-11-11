10cee8b28134673e0982abde68d71d703a79f7ce
aimen el mouzammil <115547999+ael-mouz@users.noreply.github.com>
2022-11-11 01:46:27 +0100

    Add files via upload
    done for today

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ael-mouz <ael-mouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:28:10 by ael-mouz          #+#    #+#             */
-/*   Updated: 2022/11/11 01:17:32 by ael-mouz         ###   ########.fr       */
+/*   Updated: 2022/11/11 01:44:08 by ael-mouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,23 @@ static	int	ft_check(char str, va_list p)
 	count = 0;
 	if (str == 'c')
 		count += ft_putchar(va_arg(p, int));
-	if (str == 's')
+	else if (str == 's')
 		count += ft_putstr(va_arg(p, char *));
-	if (str == 'u')
+	else if (str == 'u')
 		count += ft_putnbr(va_arg(p, unsigned int));
-	if (str == 'd' || str == 'i')
+	else if (str == 'd' || str == 'i')
 		count += ft_putnbr(va_arg(p, int));
-	if (str == 'x' || str == 'X')
+	else if (str == 'x' || str == 'X')
 		count += ft_puthx(va_arg(p, unsigned int), str);
-	if (str == 'p')
+	else if (str == 'p')
 	{
 		count += ft_putstr("0x");
 		count += ft_putadd(va_arg(p, size_t));
 	}
-	if (str == '%')
+	else if (str == '%')
 		count += ft_putchar('%');
+	else
+		count += ft_putchar(str);
 	return (count);
 }
 
@@ -51,12 +53,13 @@ int	ft_printf(const char *str, ...)
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
-			l += ft_check(str[++i], p);
-		else if (str[i] != '%')
 		{
-			write(1, &str[i], 1);
-			l++;
+			if (str[i + 1] == '\0')
+				break ;
+			l += ft_check(str[++i], p);
 		}
+		else
+			l += write(1, &str[i], 1);
 		i++;
 	}
 	va_end(p);
