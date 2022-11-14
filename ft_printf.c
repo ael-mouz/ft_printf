37e5f25fc785877b1e5faa4a8322b3cc9fbdca58
aimen el mouzammil <115547999+ael-mouz@users.noreply.github.com>
2022-11-14 19:56:53 +0100

    Add files via upload
    last version

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ael-mouz <ael-mouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:28:10 by ael-mouz          #+#    #+#             */
-/*   Updated: 2022/11/14 13:00:22 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/14 19:45:41 by ael-mouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,6 @@ static	int	ft_check(char str, va_list p)
 	}
 	else if (str == '%')
 		count += ft_putchar('%');
-	else
-		count += ft_putchar(str);
 	return (count);
 }
 
